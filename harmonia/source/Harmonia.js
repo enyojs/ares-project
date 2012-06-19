2e7cf7da0a6048ea2d9c0c227ee5546482689071
mbessey <mark.bessey@palm.com>
2012-06-18 22:29:59 -0700

    Pick up the right URL when switching providers
    Still needs a refactoring, but at least the Url ends up set correctly
    now.

--- harmonia/source/Harmonia.js
+++ harmonia/source/Harmonia.js
@@ -9,10 +9,13 @@
 		//this.log(inInfo.service.auth);
 		// super hack
 		var auth = inInfo.service ? inInfo.service.auth : null;
+		var url = inInfo.service ? inInfo.service.url : null;
 		if (this.owner) {
 			this.owner.$.service.auth = auth;
+			this.owner.$.service.setUrl(url);
 		}
 		this.$.hermesFileTree.$.service.auth = auth;
+		this.$.hermesFileTree.$.service.setUrl(url);
 		this.$.hermesFileTree.reset();
 	}
 });
