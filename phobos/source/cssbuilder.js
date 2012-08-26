045f1a739a526b4728dfe74ef1607062271a1dbb
john McConnell <johnmcconnell@yahoo.com>
2012-08-25 21:31:23 -0400

    Add scroller and font size picker

--- phobos/source/cssbuilder.js
+++ phobos/source/cssbuilder.js
@@ -1,62 +1,67 @@
 enyo.kind({
-published: {
+	published: {
 	red: '00',
 	blue: '00',
 	green: '00',
 	color: "000000",
 	toggle: ""
-},
+	},
 	name: "cssBuilder",
-	kind: "enyo.Scroller",
-	components: [
-	{kind: "enyo.FittableRows",
-	//classes: "border panel ",
-	//style: "margin: 10px;",
+	kind: "enyo.FittableRows",
 	components: [
 		{kind: "onyx.Input", name: "input", placeholder: "Enter your class name!..",onchange: "inputChange"},
+
 		{name:"outputBox",
+		kind: "enyo.Panels",
 		style: "font-family: fontFamily; font-size: 10px; border: 1px solid #000000; width: 100%; height: 150px;",
 		classes: "enyo-selectable",
 		allowHtml: true,
 		Xstyle: "padding: 10px;",
 		components: [
 			{name: "bg", allowHtml: true, style: "font-size: 12px; font-Family: fontFamily", content: ""},
-			{name: "dud",allowHtml: true, content:"  ",style: "height: 10px"},
+			{name: "dud",allowHtml: true, content:"  ",style: "height: 10px"}
 		]},
 
-		{kind: "onyx.Slider",
-		name: "redSlider",
-		onChanging: "redSliding",
-		onChange: "redChanged",
-		style: "height:10px; background-color: red; enyo-unselectable;"
-		},
 
-		{style: "height: 5px"},
 
-		{kind: "onyx.Slider",
-		name: "greenSlider",onChanging: "greenSliding",
-		onChange: "greenChanged",
-		style: "height:10px;  background-color: green; enyo-unselectable"
-		},
+		{kind: "Panels", fit:true,
+		components: [
+			{kind: "enyo.Scroller", classes: "enyo-fit",
+			components: [
+				{kind: "onyx.Slider",
+			name: "redSlider",
+			onChanging: "redSliding",
+			onChange: "redChanged",
+			style: "height:10px; background-color: red; enyo-unselectable;"
+			},
 
-			{style: "height: 5px"},
+				{style: "height: 5px"},
 
-			{kind: "onyx.Slider",
+				{kind: "onyx.Slider",
+			name: "greenSlider",onChanging: "greenSliding",
+			onChange: "greenChanged",
+			style: "height:10px;  background-color: green; enyo-unselectable"
+			},
+
+				{style: "height: 5px"},
+
+				{kind: "onyx.Slider",
 			name: "blueSlider",onChanging: "blueSliding",
 			onChange: "blueChanged",
 			style: "height:10px;  background-color: blue; enyo-unselectable"
 			},
 
-			{style: "height: 5px"},
+				{style: "height: 5px"},
 
-			{kind: "onyx.RadioGroup",
+				{kind: "onyx.RadioGroup",
 			onActivate:"radioActivated",
 			components:[
 				{content:"background", active: true, style: "width: 135px; height: 40px"},
-				{content:"Font color", style: "width: 135px; height: 40px"},
+				{content:"Font color", style: "width: 135px; height: 40px"}
 			]},
-			{tag: "br"},
-			{kind: "onyx.RadioGroup",
+				{tag: "br"},
+
+				{kind: "onyx.RadioGroup",
 			//style:
 			//width: "50 px",
 			onActivate:"fontActivated",
@@ -74,45 +79,62 @@ published: {
 				{tag: "br"},
 				{tag: "br"},
 				{content:" Courier New ", style: "font-family: Courier New; width: 140px; height: 40px"},
-				{content:" Arial ", style: "font-family: Arial; width: 140px; height: 40px"},
+				{content:" Arial ", style: "font-family: Arial; width: 140px; height: 40px"}
 
 			]},
-		]},
+
+				{classes: "onyx-toolbar-inline", components: [
+					{content: "Font Size",},
+					{kind: "onyx.PickerDecorator", components: [
+						{style: "min-width: 60px;"},
+						{name: "integerPicker", kind: "onyx.Picker",onSelect: "fontsize"}
+					]}
+				]},
+			]}
+		]}
 	],
 
 
 	create: function() {
+
 		this.inherited(arguments);
 		this.updateBox();
+				for (var i=1; i<50; i++) {
+			this.$.integerPicker.createComponent({content: i, active: !i});
+		}
 	},
 
 	updateBox: function(){
 	var tab = "&nbsp;&nbsp;&nbsp;&nbsp;";
 	var className = ".classname";
+	var fontSize = "";
 	var outPut = this.className + " " + "{<br>" ;
 	var c = '#' + (this.red + this.green + this.blue).toUpperCase();
 
-		if(this.toggle == "background"){
-			this.$.outputBox.applyStyle("background-color", c);
-			this.backgroundColor = c;
-			}
-		if(this.toggle == "Font color"){
-			this.$.outputBox.applyStyle("color", c);
-			this.fontColor = c;
-			}
+	if(this.toggle == "background"){
+		this.$.outputBox.applyStyle("background-color", c);
+		this.backgroundColor = c;
+	}
+	if(this.toggle == "Font color"){
+		this.$.outputBox.applyStyle("color", c);
+		this.fontColor = c;
+	}
 
-		if (this.backgroundColor != null){
-			outPut = outPut + tab + "background-color:" + " " +this.backgroundColor + ";";
-			}
+	if (this.backgroundColor != null){
+		outPut = outPut + tab + "background-color:" + " " +this.backgroundColor + ";";
+	}
 
-		if (this.fontColor != null){
-			outPut = outPut + "<br>" + tab + "color:" + " " + this.fontColor + ";";
-			}
+	if (this.fontColor != null){
+		outPut = outPut + "<br>" + tab + "color:" + " " + this.fontColor + ";";
+	}
 
-		if (this.fontFamily != null){
-			outPut = outPut + "<br>" +tab + "font-family:" + " " +this.fontFamily + ";";
-		}
-		this.$.bg.setContent(outPut + "<br>}");
+	if (this.fontFamily != null){
+		outPut = outPut + "<br>" +tab + "font-family:" + " " +this.fontFamily + ";";
+	}
+	if (this.$.fontSize != null){
+		outPut = outPut + "<br>" +tab + "font-size:" + " " +this.$.fontSize + "px;";
+	}
+	this.$.bg.setContent(outPut + "<br>}");
 	},
 
 	redChanged: function(inSender, inEvent){
@@ -191,6 +213,13 @@ published: {
 	},
 	inputChange: function(inSender, inEvent){
 		this.className = this.$.input.hasNode().value;
+		this.updateBox();
+	},
+	fontsize: function(inSender, inEvent) {
+
+	//this.$.fontSize = "";
+		this.$.fontSize = inEvent.selected.content;
+		console.log(this.$.fontSize);
 		this.updateBox();
 	}
 });
