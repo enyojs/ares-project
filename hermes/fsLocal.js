74366b19b98c11584c558dd781d6ddbd7a2cafcb
Marie-Antoinette (aka Marian) de Bonis-Hamelin <marie-antoinette.de-bonis-hamelin@hp.com>
2012-10-16 19:31:41 +0200

    just a temporary version

--- hermes/fsLocal.js
+++ hermes/fsLocal.js
@@ -28,6 +28,7 @@ function FsLocal(config, next) {
 
 	// (simple) parameters checking
 	config.root = path.resolve(config.root);
+	console.dir(config);
 
 	var app = express.createServer();
 	app.use(express.logger('dev'));
@@ -173,6 +174,7 @@ function FsLocal(config, next) {
 			origin: "http://127.0.0.1:"+app.address().port.toString(),
 			pathname: config.pathname
 		};
+		//console.dir(service);
 		return next(null, service);
 	});
 
@@ -566,10 +568,23 @@ function FsLocal(config, next) {
 }
 
 //debugger;
-
+var toto = process.argv;
 if (path.basename(process.argv[1]) === "fsLocal.js") {
 	// We are main.js: create & run the object...
+	
+	var argv = require("optimist")
+    .options('P', {
+        alias : 'pathname',
+        default : '/some/files'
+    })
+    .options('p', {
+        alias : 'port',
+        default : '9009',
+    })
+	.argv;
 
+	console.dir(argv);
+	
 	var version = process.version.match(/[0-9]+.[0-9]+/)[0];
 	if (version <= 0.7) {
 		process.exit("Only supported on Node.js version 0.8 and above");
@@ -577,16 +592,16 @@ if (path.basename(process.argv[1]) === "fsLocal.js") {
 
 	var fsLocal = new FsLocal({
 		// pathname (M) can be '/', '/res/files/' ...etc
-		pathname:	process.argv[2],
+		pathname:	argv.pathname,
 		// root (m) local filesystem access root absolute path
-		root:		path.resolve(process.argv[3]),
+		root:		argv._[0],
 		// port (o) local IP port of the express server (default: 9009, 0: dynamic)
-		port: parseInt(process.argv[4] || "9009", 10)
+		port: 	argv.port
 	}, function(err, service){
 		if (err) {
 			process.exit(err);
 		}
-		console.log("fsLocal['"+process.argv[3]+"'] available at "+service.origin);
+		console.log("fsLocal['"+argv._[1]+"'] available at "+service.origin);
 		if (process.send) {
 			// only possible/available if parent-process is node
 			process.send(service);
