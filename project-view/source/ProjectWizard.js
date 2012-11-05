258d901a2c4e46cc6b8a782053bb5bae28c68b70
Dominique Dumont <dominique.dumont@hp.com>
2012-11-05 17:01:27 +0100

    ENYO-1156: import project is working
    Enyo-DCO-1.0-Signed-off-by: Dominique Dumont <dominique.dumont@hp.com>

--- project-view/source/ProjectWizard.js
+++ project-view/source/ProjectWizard.js
@@ -140,78 +140,75 @@ enyo.kind({
 	},
 	components: [
 	    {kind: "FittableRows", fit: true, components: [
-			{kind: "FittableColumns", components: [
-				{kind: "Control", content: "Project name: "},
-				{kind: "onyx.InputDecorator", components: [
-					{kind: "Input", defaultFocus: true, placeholder: "Enter project name", name: "projectName", oninput: "updateProjectDir"}
-				]}
-			]},
+			{kind: "Control", content:"Select a location containing one or mode project.json"},
+			{fit: true},
 			{kind: "FittableColumns", components: [
 				{kind: "Control", content: "Location:"},
 				{kind: "Control", content: "", name: "projectLocation", fit: true},
 				{kind: "onyx.Button", content: "Browse", ontap: "showDirPopup"}
 			]},
-			{kind: "FittableColumns", name: "fittableColumns4", components: [
-				{kind: "Control", content: "Project directory:"},
-				{kind: "onyx.InputDecorator", components: [
-					{kind: "onyx.Input", name: "projectDirectory", onchange: "enableConfirmButton"}
-				]}
-			]},
 			{fit: true},
 			{kind: "FittableColumns", style: "margin-top: 10px", name: "fittableColumns5", components: [
-				{kind: "Control", fit: true},
 				{kind: "onyx.Button", content: "Cancel", ontap: "hide"},
-				{kind: "onyx.Button", content: "OK", name: "confirm", ontap: "confirmTap"}
+				{kind: "Control", fit: true},
+				{kind: "onyx.Button", content: "OK", name: "confirm", ontap: "searchProjects"}
 			]}
 		]},
 		{kind: "SelectDirectoryPopup", canGenerate: false, name: "selectDirectoryPopup", onCancel: "cancelDirSelection"}
     ],
-	// false when wizard is invoked through import project
-    createMode: true,
     debug: false,
     reset: function() {
-    	this.$.projectName.setValue("");
-    	this.$.projectDirectory.setValue("");
     	this.$.confirm.setDisabled(true);
 		return this ;
-    },
-    setCreateMode: function(createMode) {
-    	this.createMode = createMode;
     },
     cancelDirSelection: function() {
     	this.$.selectDirectoryPopup.hide();
     },
-	createProject: function (name, folderId, subDir) {
-		var service = this.selectedDir.service;
-		var matchingNode ;
-        var matchFileName = function(node,name){
-			return (n.name === name) ;
-		};
-		folderId = this.selectedDir.id;
-		matchingNode = service.getNodesFiles.filter( matchFileName ) ;
-			
-		if (this.debug) this.log("Creating new folder " + name + " into folderId=" + folderId);
-
-		service.createFolder(folderId, name)
-			.response(this, function(inSender, inResponse) {
-				this.importProject(name, folderId) ;
-			})
-			.error(this, function(inSender, inError) {
-				// TODO handle the error
-				this.log("Error: "+inError);
-				this.doCancel();
-			});
-	},
-	confirmTap: function(inSender, inEvent) {
-		var name = this.$.projectName.getValue();
-		var subDir = this.$.projectDirectory.getValue() ;
+	searchProjects: function (inSender, inEvent) {
 		var folderId = this.selectedDir.id ;
-    	if (this.createMode === true) {
-			this.createProject(name, folderId, subDir) ;
-    	} else {
-			this.importProject(name, this.selectedDir.service, subDir) ;
-    	}
-    },
+		var service = this.selectedDir.service;
+		var hft = this.$.selectDirectoryPopup.$.hermesFileTree ;
+
+		// we cannot use directly listFiles as this method sends a list of unrelated files
+		// we need to use the file tree to be able to relate a project.json with is parent dir.
+		var topDir = hft.selectedNode.file ;
+
+		// construct an (kind of) iterator that will scan all directory of the 
+		// HFT and look for project.json
+		var toScan = [ [ null , topDir ] ]  ; // list of parent_dir , child
+		// var this = this ;
+
+		var iter = function() {
+		    while (toScan.length > 0) {
+				var item = toScan.shift() ;
+				var parentDir = item[0] ;
+				var child = item[1];
+				this.log('myiter on ' + child.name ) ;
+				if ( child.name === 'project.json' ) { 
+					this.log('opening project.json from ' + parentDir.name ) ;
+					service.getFile( child.id ).
+						response(this, function(inSender, inContent) {
+							this.log( inContent ) ;
+							this.log('Imported project ' + inContent.content.name ) ;
+							this.addProjectInList(inContent.content.name, parentDir.id) ;
+						}); 
+				}
+				if ( child.isDir ) { 
+					service.listFiles(child.id)
+						.response(this, function(inSender, inFiles) {
+							enyo.forEach(inFiles, function(v) {
+								this.log('pushing ' + v.name) ;
+								toScan.push([child,v]);
+							},this) ;
+							iter.apply(this) ;
+						}
+					) ;
+				}
+			}
+		} ;
+
+		iter.apply(this) ; // oops. forgot to launch the whole stuff
+	},
     directorySelected: function(inSender, inEvent) {
     	this.selectedServiceId = inEvent.serviceId;
     	this.selectedDir = inEvent.directory;
@@ -219,15 +216,8 @@ enyo.kind({
     	this.enableConfirmButton();
     	this.$.selectDirectoryPopup.hide();
     },
-    updateProjectDir: function() {
-		this.$.projectDirectory.setValue(this.$.projectName.getValue()) ;
-		this.enableConfirmButton() ;
-	},
     enableConfirmButton: function() {
-    	if (    this.$.projectDirectory.getValue() 
-			 && this.$.projectName     .getValue() 
-			 && this.$.projectLocation .getContent()
-		) {
+    	if ( this.$.projectLocation .getContent()) {
     		this.$.confirm.setDisabled(false);
 		} else {
 			this.$.confirm.setDisabled(true);
