93bacc03fc00afe4ec5162d5cc2de96491005391
Dominique Dumont <dominique.dumont@hp.com>
2012-11-23 15:40:17 +0100

    ENYO-1222: rewrote project setting widget
    Use Drawers to show/close project and phonegap settings
    
    Enyo-DCO-1.1-Signed-off-by: Dominique Dumont <dominique.dumont@hp.com>

--- project-view/source/ProjectProperties.js
+++ project-view/source/ProjectProperties.js
@@ -1,3 +1,11 @@
+/**
+ * This kind provide a widget to tune project properties (phonegap
+ * stuff included).
+ * 
+ * By default, this widget is tuned for project modification.  In case
+ * of project *creation*, the method setup_create must be called after
+ * construction.
+ */
 enyo.kind({
 	name: "ProjectProperties",
 	classes: "enyo-unselectable",
@@ -8,68 +16,124 @@ enyo.kind({
 		onCanceled: "",
 	},
 	createMode: true,
+
 	components: [
-		{kind: "FittableRows", fit: true, components: [
-			{content: "Settings", style: "width:100%"},
-			{tag: "br"},
-			{kind: "FittableColumns", style: "width:100%", components: [
-					{kind: "Control", content: "Project Id:"},
-					{fit: true},
-					{kind: "onyx.InputDecorator", components: [
-							{kind: "onyx.Input", style: "width:180px", placeholder: "com.example.myapp", name: "projectId"}
-						]},
-				]},
-			{kind: "FittableColumns", style: "width:100%", components: [
-					{kind: "Control", content: "Project Name:"},
-					{fit: true},
-					{kind: "onyx.InputDecorator", components: [
-							{kind: "onyx.Input", placeholder: "My Project Name", name: "projectName"}
-					]},
-				]},
-			{tag: "br"},
-			{kind: "FittableColumns", style: "width:100%", components: [
-					{kind: "Control", content: "Title:"},
-					{fit: true},
-					{kind: "onyx.InputDecorator", components: [
-							{kind: "onyx.Input", style: "width:230px", placeholder: "", name: "projectTitle"}
-					]},
-				]},
-			{kind: "FittableColumns", style: "width:100%", components: [
-					{kind: "Control", content: "Description:"},
-					{fit: true},
-					{kind: "onyx.InputDecorator", components: [
-							{kind: "onyx.Input", style: "width:180px", placeholder: "", name: "projectDescription"}
-					]},
-				]},
-			{content: "PhoneGap ...", style: "width:100%", ontap:"activateDrawer"},
-			{tag: "br"},
-					{name: "phoneGapDrawer", kind: "onyx.Drawer", open:false, components: [
-					{kind: "FittableColumns", style: "width:100%", components: [
-						{kind: "Control", content: "Target Build:"},
-						{fit: true},
-						{kind: "onyx.InputDecorator", components: [
-							{kind: "onyx.Input", placeholder: "Enter a target build", name: "targetBuild"}
-						]},
-					]}, 
-				]},
-			{name: "status", tag: "p", content: "You need to create a project!", style: "width:100%; color: red"},
-			{kind: "FittableColumns", style: "width:100%", components: [
-					{kind: "Control"},
-					{fit: true},
-					{kind: "onyx.Button", content: "Cancel", classes: "onyx-negative", name: "cancel", ontap: "doCanceled"},
-					{kind: "onyx.Button", content: "OK", classes: "onyx-affirmative", name: "confirm", ontap: "confirmTap"}
-				]}
+		{kind: "onyx.RadioGroup", onActivate: "switchDrawers", name: "thumbnail", components: [
+			{content: "Project", active: true, attributes: {title: 'project attributes...'}},
+			{content: "PhoneGap", attributes: {title: 'phonegap build parameters...'}}
 		]},
+		{name: "projectDrawer", kind: "onyx.Drawer", open:true, components: [
+			{content: "Project", tag:"h2"},
+			{tag: 'table', components: [
+				{tag: "tr" , components: [
+					 {tag: "td" , content: "Name: "},
+					 {tag: 'td', components:[
+						  {kind: "onyx.InputDecorator", components: [
+							   {kind: "Input", defaultFocus: true, name: "projectName"}
+						  ]}
+					 ]},
+					 {tag: 'td', content: "Title: "},
+					 {tag: "td" , components: [
+						  {kind: "onyx.InputDecorator", components: [
+							   {kind: "Input", defaultFocus: true, name: "projectTitle"}
+						   ]}
+					  ]}
+				]},
+				{tag: "tr" , components: [
+					 {tag: "td" , content: "Version: "},
+					 {tag: 'td', components:[
+						  {kind: "onyx.InputDecorator", components: [
+							   {kind: "Input", defaultFocus: true, name: "projectVersion"}
+						  ]}
+					 ]},
+					 {tag: 'td', content: "Id: "},
+					 {tag: "td" , components: [
+						  {kind: "onyx.InputDecorator", components: [
+							   {kind: "Input", defaultFocus: true, name: "projectId", 
+								attributes: {title: "Application ID in reverse domain-name format: com.example.apps.myapp"}}
+						   ]}
+					  ]}
+				]},
+				{tag: "tr" , name:'directoryEntry', canGenerate:false, components: [
+					 {tag: "td", content: "Directory: "},
+					 {tag: 'td', attributes: {colspan: 3}, content: "", name: "projectDirectory" }
+				]}
+			]}
+		]},
+		// FIXME: use true for debug
+		{name: "phoneGapDrawer", kind: "onyx.Drawer", open: false, components: [
+			{content: "PhoneGap", tag:"h2"},
+			{kind: "onyx.ToggleButton", onContent: "enabled", offContent: "disabled", onChange: "togglePhonegap"},
+			{tag: 'table', attributes: {'class': 'ares_projectView_table'}, components: [
+				{tag: "tr" , components: [
+					 {tag: "td" , content: "AppId: "},
+					 {tag: 'td', attributes: {colspan: 3}, components:[
+						  {kind: "onyx.InputDecorator", components: [
+							   {kind: "Input", name: "phonegapId", placeholder: "com.example.myapp"}
+						   ]}
+					 ]}
+				]},
+				{tag: "tr" , components: [
+					 {tag: 'td', content: "PhoneGap targets", attributes: {colspan: 3}}
+				]},
+				{tag: "tr" , components: [
+					{tag: "td" , components: [
+						  {kind: "onyx.ToggleButton", onContent: "Android", offContent: "Android", onChange: "togglePhonegapTarget"}
+					]},
+					{tag: "td" , components: [
+						  {kind: "onyx.ToggleButton", onContent: "Ios", offContent: "Ios", onChange: "togglePhonegapTarget"}
+					]},
+					{tag: "td" , components: [
+
+						  {kind: "onyx.ToggleButton", onContent: "Winphone", offContent: "Winphone", onChange: "togglePhonegapTarget"}
+					]}
+				]},
+				{tag: "tr" , components: [
+					{tag: "td" , components: [
+						  {kind: "onyx.ToggleButton", onContent: "Blackberry", offContent: "Blackberry", onChange: "togglePhonegapTarget"}
+					]},
+					{tag: "td" , components: [
+						  {kind: "onyx.ToggleButton", onContent: "Webos", offContent: "Webos", onChange: "togglePhonegapTarget"}
+					]}
+				]}
+			]}
+		]},
+
+		// FIXME: there should be an HTML/CSS way to avoid using FittableStuff...
+		{kind: "FittableRows", style: "margin-top: 10px; width: 100%", fit: true, components: [
+			{kind: "onyx.Button", classes: "onyx-negative", content: "Cancel", ontap: "doCanceled"},
+			{name: "ok", kind: "onyx.Button", classes: "onyx-affirmative", content: "OK", ontap: "createProject"}
+		]},
+
+		{kind: "Ares.ErrorPopup", name: "errorPopup", msg: "unknown error"}
 	],
+
 	debug: false,
-	create: function() {
-		this.inherited(arguments);
-		this.$.confirm.setDisabled(true);
+
+	/**
+	 * Tune the widget for project creation
+	 */
+	setup_create: function() {
+		this.$.ok.setDisabled(true) ;
+		this.$.directoryEntry.show() ;
 	},
-	activateDrawer: function() {
-		//drawer is closed
-		this.$.phoneGapDrawer.setOpen(!this.$.phoneGapDrawer.open);
+
+	/**
+	 * close one drawer and open the other depending on which radio button was tapped
+	 */
+	switchDrawers: function(inSender, inEvent) {
+		if (inEvent.originator.active === true ) {
+			if (inEvent.originator.getContent() === "Project") {
+				this.$.projectDrawer.setOpen(true) ;
+				this.$.phoneGapDrawer.setOpen(false) ;
+			}
+			else {
+				this.$.projectDrawer.setOpen(false) ;
+				this.$.phoneGapDrawer.setOpen(true) ;
+			}
+		}
 	},
+
 	reset: function(inData) {
 		this.$.confirm.setDisabled(true);
 		this.activateDrawer();
