6628f1dc7d62ad2a8768fa0a92aaa72b636f2acd
Mark Bessey <mark.bessey@palm.com>
2012-11-28 17:06:14 -0800

    Slideable fileview
    Put the project and file views on a slideable

--- ares/Ares.js
+++ ares/Ares.js
@@ -1,28 +1,42 @@
 enyo.kind({
 	name: "Ares",
-	kind: "enyo.Panels",
+	kind: "Control",
+	classes: "app onyx",
 	fit: true,
 	components: [
-		{kind: "ProjectView", onFileDblClick: "openDocument"},
-		{kind: "Phobos", onSaveDocument: "saveDocument", onCloseDocument: "closeDocument", onDesignDocument: "designDocument"},
-		{kind: "Deimos", onCloseDesigner: "closeDesigner"},
+		{kind: "Panels", /*arrangerKind: "CarouselArranger",*/ classes: "enyo-fit", components: [
+			{kind: "Phobos", onSaveDocument: "saveDocument", onCloseDocument: "closeDocument", onDesignDocument: "designDocument"},
+			{kind: "Deimos", onCloseDesigner: "closeDesigner"},
+		]},
+		{kind: "Slideable", style: "height: 100%; width: 100%", layoutKind: "FittableRowsLayout", classes: "onyx", axis: "v", value: 0, min: -500, max: 0, unit: "px", components: [
+			{kind: "ProjectView", fit: true, classes: "onyx", onFileDblClick: "openDocument"},
+			{kind: "onyx.MoreToolbar", components: [
+				{kind: "onyx.Grabber", ontap: "toggleFiles"},
+				{kind: "onyx.RadioGroup", components: [
+				    {content: "file.txt"}
+				]}
+			]}
+		]},
 		{kind: "ServiceRegistry"}
 	],
-	//arrangerKind: "CollapsingArranger",
 	handlers: {
 		onReloadServices: "handleReloadServices"
 	},
-	projectViewIndex: 0,
-	phobosViewIndex: 1,
-	deimosViewIndex: 2,
+	phobosViewIndex: 0,
+	deimosViewIndex: 1,
 	create: function() {
 		this.inherited(arguments);
-		this.setIndex(this.projectViewIndex);
+		this.$.panels.setIndex(this.phobosViewIndex);
 
 		window.onbeforeunload = enyo.bind(this, "handleBeforeUnload");
 		if (this.runTest) {
 			this.createComponent({kind: "ares.TestConsoleRunner", reporterKind: "ares.TestConsoleReporter"});
 		}
+		this.calcSlideableLimit();
+	},
+	rendered: function() {
+		this.inherited(arguments);
+		this.calcSlideableLimit();
 	},
 	draggable: false,
 	handleReloadServices: function(inSender, inEvent) {
@@ -44,7 +58,8 @@ enyo.kind({
 					inData="";
 				}
 				this.$.phobos.openDoc(origin, f, inData, ext, projectUrl);
-				this.setIndex(this.phobosViewIndex);
+				this.$.panels.setIndex(this.phobosViewIndex);
+				this.hideFiles();
 			})
 			.error(this, function(inEvent, inData) {
 				enyo.log("Open failed", inData);
@@ -63,21 +78,45 @@ enyo.kind({
 			});
 	},
 	closeDocument: function(inSender, inEvent) {
-		this.setIndex(this.projectViewIndex);
+		this.showFiles();
 	},
 	designDocument: function(inSender, inEvent) {
 		this.$.deimos.load(inEvent);
-		this.setIndex(this.deimosViewIndex);
+		this.$.panels.setIndex(this.deimosViewIndex);
 	},
 	closeDesigner: function(inSender, inEvent) {
 		if (inEvent.docHasChanged) {
 			this.$.phobos.updateComponents(inSender, inEvent);
 		}
-		this.setIndex(this.phobosViewIndex);
+		this.$.panels.setIndex(this.phobosViewIndex);
 	},
 	handleBeforeUnload: function() {
 		if (window.location.search.indexOf("debug") == -1) {
 			return 'You may have some unsaved data';
 		}
+	},
+	hideFiles: function(inSender, inEvent) {
+		this.$.slideable.animateToMin();
+	},
+	showFiles: function(inSender, inEvent) {
+		this.$.slideable.animateToMax();
+	},
+	toggleFiles: function(inSender, inEvent) {
+		if (this.$.slideable.value < 0) {
+			this.showFiles();
+		} else {
+			this.hideFiles();
+		}
+	},
+	resizeHandler: function(inSender, inEvent) {
+		this.inherited(arguments);
+		this.calcSlideableLimit();
+		if (this.$.slideable.value < 0) {
+			this.$.slideable.setValue(this.$.slideable.min);
+		}
+	},
+	calcSlideableLimit: function() {
+		var min = this.getBounds().height-this.$.moreToolbar.getBounds().height;
+		this.$.slideable.setMin(-min);
 	}
 });
