c22120258494cf0bced881b53b3bfddd824a042d
John McConnell <johnmcconnell@yahoo.com>
2013-01-02 19:29:04 -0500

    enyo 1769
    remove two fit and add a close button

--- phobos/source/FindPopup.js
+++ phobos/source/FindPopup.js
@@ -5,7 +5,8 @@ enyo.kind({
 		onFindNext:"",
 		onFindPrevious:"",
 		//onReplace:"",
-		onReplaceAll:""
+		onReplaceAll:"",
+		onClose: ""
 	},
 	published: {
 		findValue:"",
@@ -17,14 +18,14 @@ enyo.kind({
 	components: [
 		{kind: "FittableRows", classes:"ares_phobos_findpop", components: [
 			{kind: "FittableColumns", components: [
-				{fit: true, content: "Find:", classes: "phobos-find-label"},
+				{content: "Find:", classes: "phobos-find-label"},
 				{kind: "onyx.InputDecorator", components: [
 					{name: "find", kind: "onyx.Input", classes: "phobos-find-field", placeholder: "", onchange: "findChanged"}
 				]},
 			]},
 			{tag: "br"},
 			{kind: "FittableColumns", components:[
-				{fit: true, content: "Replace:", classes: "phobos-find-label"},
+				{content: "Replace:", classes: "phobos-find-label"},
 				{kind: "onyx.InputDecorator", components: [
 					{name: "replace", kind: "onyx.Input", classes: "phobos-find-field", placeholder: "", onchange: "replaceChanged"}
 				]},
@@ -32,6 +33,7 @@ enyo.kind({
 			{tag: "br"},
 			{kind: "FittableColumns", components: [
 				//{name: "replaceOne", kind: "onyx.Button", content: "Replace", ontap: "doReplace"},
+				{name: "close", kind: "onyx.Button", content: "Close", ontap: "doClose"},
 				{name: "replaceAll", kind: "onyx.Button", content: "Replace All", ontap: "doReplaceAll"},
 				{style: "width: 20px"},
 				{name: "findprevious", kind: "onyx.Button", content: "Previous", ontap: "doFindPrevious"},
