4d96d40544de06cbaa3f0203aa7c1af48b06acc0
Dominique Dumont <dominique.dumont@hp.com>
2013-01-10 11:30:08 +0100

    ENYO-1710: take into account device DPR to compute the iframe size

--- preview/source/Preview.js
+++ preview/source/Preview.js
@@ -35,13 +35,13 @@ enyo.kind(
 							{
 								kind: "onyx.Picker", name: "device",
 								components: [
-									{content: "default",           value: { height:  800, width:  600, ppi: 163 }, active: true},
-									{content: "iPhone\u2122",      value: { height:  480, width:  320, ppi: 163 }},
-									{content: "iPhone\u2122 4",    value: { height:  940, width:  660, ppi: 326 }},
-									{content: "iPhone\u2122 5",    value: { height: 1146, width:  640, ppi: 326 }},
-									{content: "iPad\u2122 Retina", value: { height: 2048, width: 1536, ppi: 264 }},
-									{content: "iPad\u2122 2",      value: { height: 1280, width:  800, ppi: 132 }},
-									{content: "iPad\u2122 mini",   value: { height: 1024, width:  768, ppi: 163 }}
+									{content: "default",           value: { height:  800, width:  600, ppi: 163, dpr: 1 }, active: true},
+									{content: "iPhone\u2122",      value: { height:  480, width:  320, ppi: 163, dpr: 1 }},
+									{content: "iPhone\u2122 4",    value: { height:  940, width:  660, ppi: 326, dpr: 1 }},
+									{content: "iPhone\u2122 5",    value: { height: 1146, width:  640, ppi: 326, dpr: 1 }},
+									{content: "iPad\u2122 Retina", value: { height: 2048, width: 1536, ppi: 264, dpr: 2 }},
+									{content: "iPad\u2122 2",      value: { height: 1280, width:  800, ppi: 132, dpr: 1 }},
+									{content: "iPad\u2122 mini",   value: { height: 1024, width:  768, ppi: 163, dpr: 1 }}
 								]
 							}
 						]
@@ -67,8 +67,8 @@ enyo.kind(
 			var device = this.$.device.selected ;
 			var orientation = this .$.orientation.selected ;
 
-			var dw = device.value.width ;
-			var dh = device.value.height ;
+			var dw = device.value.width / device.value.dpr;
+			var dh = device.value.height / device.value.dpr;
 			this.dlog("size for device " , device.content , " orientation " , orientation.content ) ;
 			var swap = orientation.swap ;
 			this.$.iframe.setGeometry( swap ? dh : dw, swap ? dw : dh) ;
