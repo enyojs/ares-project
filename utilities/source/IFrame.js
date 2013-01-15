97128850599032330c714dc209f8fe7bb694168f
Dominique Dumont <dominique.dumont@hp.com>
2013-01-15 16:50:10 +0100

    ENYO-1710-bonus: attempt to make bezel draggable to zoom the preview
    The code sort-of work to zoom the preview with the bottom bar. But
    fine tuning the zoom and position of the bezel+iframe is too time
    consuming. Or I don't have the right algorithm.

--- utilities/source/IFrame.js
+++ utilities/source/IFrame.js
@@ -10,7 +10,7 @@ enyo.kind(
 			height: '800px'
 		},
 
-		classes: "enyo-border-box ares-preview-device",
+		classes: "enyo-border-box",
 
 		//style: " overflow: hidden;",
 
@@ -41,8 +41,16 @@ enyo.kind(
 
 		components: [
 			{
-				kind: "ares.IFrame",
-				name: 'iframe'
+				classes: "ares-preview-device",
+				name: "bezel" ,
+				ondrag: "handleDrag",
+
+				components: [
+					{
+						kind: "ares.IFrame",
+						name: 'iframe'
+					}
+				]
 			}
 		],
 
@@ -52,6 +60,68 @@ enyo.kind(
 		setGeometry: function(width, height) {
 			this.$.iframe.setGeometry( width, height) ;
 			this.resized() ;
+		},
+
+		scale: 1 ,
+		shiftX: 0,
+		shiftY: 0,
+
+		handleDragStart: function(inSender, inEvent) {
+			var frame = this.$.iframe.getBounds() ;
+			this.startH = frame.height;
+			this.startW = frame.width;
+		},
+
+		handleDrag: function(inSender, inEvent) {
+			this.log(inSender, inEvent) ;
+			this.log( 'pageX ' + inEvent.pageX + ' ddx ' + inEvent.ddx );
+			this.log( 'pageY ' + inEvent.pageY + ' ddy ' + inEvent.ddy );
+			var x0 = this.getBounds().left ;
+			var y0 = this.getBounds().top ;
+			var frame = this.$.iframe.getBounds() ;
+			var coeff = 1;
+
+			var topY = y0 + frame.top * this.scale ;
+			var bottomY = y0 + (frame.top + frame.height) * this.scale ;
+			var pY = inEvent.pageY ;
+
+			this.log('drag pY ' + pY + ' topY ' + topY + ' bottomY ' + bottomY ) ;
+
+			if (pY < topY ) {
+				// in top bar
+				//coeff = ( frame.height - 2*inEvent.ddy ) / frame.height ;
+				this.shiftY += inEvent.ddy ;
+			}
+			else if (pY > bottomY) {
+				// in bottom bar
+				coeff = ( frame.height + inEvent.ddy ) / frame.height ;
+				this.shiftY += inEvent.ddy/2 ;
+			}
+			else if (inEvent.pageY < y0 + frame.left ) {
+				// in top bar
+			}
+			else if (inEvent.pageY > y0 + frame.left + frame.width) {
+				// in bottom bar
+			}
+			// implement using enyo.dom.transformValue
+			//if (inEvent.pageY > this.$)
+
+			this.scale *= coeff ;
+
+			if (this.scale > 1) {
+				this.scale = 1;
+			}
+
+			this.log('scaling to ' + this.scale + ' with coeff ' + coeff + ' shiftY ' + this.shiftY ) ;
+
+			enyo.dom.transform(
+				this.$.bezel, {
+					"scale": this.scale,
+					translate: Math.round(this.shiftX) + "px, " + Math.round(this.shiftY) + "px"
+				}
+			) ;
+			this.resized() ;
+
 		}
 
 	}
