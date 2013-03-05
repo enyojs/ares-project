1a0a2d7e7f87b341a9a8944ad0411f8f2f4da949
Marian de Bonis <marie-antoinette.de-bonis-hamelin@hp.com>
2013-03-05 17:50:19 +0100

    ENYO-1848 test/tester/main.js rewrite as installer Enyo-DCO-1.1-Signed-off-by: Marie-Antoinette de Bonis-Hamelin (Marian) marie-antoinette.de-bonis-hamelin@hp.com

--- test/tester/main.js
+++ test/tester/main.js
@@ -1,12 +1,9 @@
-#!/usr/bin/env node
-
-// Default Dependencies
 var path = require('path'),
     fs = require('fs'),
     os = require('os'),
-	shell = require('../../enyo/tools/node_modules/shelljs');
+    npmlog = require('./../../node_modules/npmlog'),
+	shell = require('./../../enyo/tools/node_modules/shelljs');
 
-// Local variables
 var srcDir = process.cwd(),
 	tDir = path.resolve(srcDir, 'test'),
 	rDir = path.resolve(tDir, 'root'),
@@ -18,37 +15,61 @@ var srcDir = process.cwd(),
 	fopsTestDir = path.resolve(srcDir, 'test/root/FileOps'),
 	fopsSrcDir = path.resolve(srcDir, 'test/samples/FileOps');
 
-// cleanup and set up the dedicated source tree either for TestRunner and Selenium
-// console.log("os.type(): "+ os.type());
-// cleanup test/root
-shell.cd(path.join(tDir));
-shell.rm('-rf', path.join(rDir));
+var enyoDir = path.resolve(tDir, '../enyo'),
+ 	layoutDir = path.resolve(tDir, '../lib/layout'),
+ 	onyxDir = path.resolve(tDir, '../lib/onyx'),
+ 	lib = path.resolve(testDir, 'HelloWorld/lib');
 
-// setup test/root and sub-directories
-shell.mkdir('-p', path.join(testDir));
-shell.mkdir('-p', path.join(newProjectDir));
-shell.mkdir('-p', path.join(helloTestDir));
-shell.mkdir('-p', path.join(fopsTestDir));
+(function() {
 
-// import the HelloWorld project and dependencies from test/samples
-shell.cp('-R', path.join(helloSrcDir), path.join(testDir));
-shell.cd(path.join(helloTestDir));
-shell.mkdir('-p', path.join(lib));
-if (os.type() === 'Windows_NT') {
-	var enyoDir = path.resolve(tDir, '../enyo'),
-	 	layoutDir = path.resolve(tDir, '../lib/layout'),
-	 	onyxDir = path.resolve(tDir, '../lib/onyx'),
-	 	lib = path.resolve(testDir, 'HelloWorld/lib');
-	shell.cp('-R', path.join(enyoDir), path.join(helloTestDir));
-	shell.cd('lib');
-	shell.cp('-R', path.join(layoutDir), path.join(lib));
-	shell.cp('-R', path.join(onyxDir), path.join(lib));
-} else {
-	// symlinks foes not exist on Windows
-	shell.exec('/bin/ln -s ../../../enyo enyo');
-	shell.exec('/bin/ln -s ../../../../lib/layout lib/layout');
-	shell.exec('/bin/ln -s ../../../../lib/onyx lib/onyx');	
-}
-// import the FileOps dir from test/samples
-shell.cd(path.join(fopsTestDir));
-shell.cp('-R', path.join(fopsSrcDir), path.join(testDir));
+        var log = npmlog;
+        log.heading = 'installer';
+        log.level = 'warn';
+
+        var tester = {
+        	setup: function(req, res, next) {
+ 				// cleanup and set up the dedicated source tree either for TestRunner and Selenium
+				// console.log("os.type(): "+ os.type());
+				// cleanup test/root
+				shell.cd(path.join(tDir));
+				shell.rm('-rf', path.join(rDir));
+
+				// setup test/root and sub-directories
+				shell.mkdir('-p', path.join(testDir));
+				shell.mkdir('-p', path.join(newProjectDir));
+				shell.mkdir('-p', path.join(helloTestDir));
+				shell.mkdir('-p', path.join(fopsTestDir));
+
+				// import the HelloWorld project and dependencies from test/samples
+				shell.cp('-R', path.join(helloSrcDir), path.join(testDir));
+				shell.cd(path.join(helloTestDir));
+				shell.mkdir('-p', path.join(lib));
+				if (os.type() === 'Windows_NT') {
+					shell.cp('-R', path.join(enyoDir), path.join(helloTestDir));
+					shell.cd('lib');
+					shell.cp('-R', path.join(layoutDir), path.join(lib));
+					shell.cp('-R', path.join(onyxDir), path.join(lib));
+				} else {
+					// symlinks foes not exist on Windows
+					shell.exec('/bin/ln -s ../../../enyo enyo');
+					shell.exec('/bin/ln -s ../../../../lib/layout lib/layout');
+					shell.exec('/bin/ln -s ../../../../lib/onyx lib/onyx');	
+				}
+				// import the FileOps dir from test/samples
+				shell.cd(path.join(fopsTestDir));
+				shell.cp('-R', path.join(fopsSrcDir), path.join(testDir));
+
+				console.log('Ares Test setup done!');
+				res.status(200);
+				next();
+			},
+			cleanup: function(req, res, next) {
+				next();
+			}
+		};
+
+        if (typeof module !== 'undefined' && module.exports) {
+                module.exports = tester;
+        }
+        
+}());
