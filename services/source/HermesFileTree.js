800511044f815ebaeef10a0c39ea3197ce734300
Kevin Schaaf <kevinpschaaf@Kevins-MacBook-Pro.local>
2013-03-10 16:30:19 -0700

    ENYO-1484: Add project-relative "dir" property to file objects.
    Enyo-DCO-1.1-Signed-off-by: Kevin Schaaf <kevin.schaaf@lge.com>

--- services/source/HermesFileTree.js
+++ services/source/HermesFileTree.js
@@ -260,6 +260,14 @@ enyo.kind({
 	},
     updateNodeContent: function(inNode, files) {
 		var i = 0 , nfiles, rfiles, res, modified = 0, newControl ;
+		// Add dir property to files, which is a project-relative path
+		enyo.forEach(files, function(f) {
+			if (f.isDir) {
+				f.dir = inNode.file.dir + f.name + "/";
+			} else {
+				f.dir = inNode.file.dir;
+			}
+		});
 		rfiles = this.filesToNodes(files) ; // with prefix in name
 		nfiles = this.getNodeFiles(inNode) ;
 
@@ -427,6 +435,11 @@ enyo.kind({
 		if (this.debug) this.log(this) ;
 		var target = node || this.$.serverNode ;
 		var inTop = 0;
+
+		if (!node) {
+			// Initialize project-relative root dir
+			target.file.dir = "/";
+		}
 
 		if (! tracker) {
 			inTop++ ;
