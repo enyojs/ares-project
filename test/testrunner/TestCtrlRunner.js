2aa154dfa31c9b3c56f4ff2df34a5adb95aa3ed8
Marian de Bonis <marie-antoinette.de-bonis-hamelin@hp.com>
2013-03-24 20:30:23 +0100

    ENYO-2040: Display the output of TestRunner into the console . no more opening a new window due to unsuported window.postMessage API Enyo-DCO-1.1-Signed-off-by: Marie-Antoinette de Bonis-Hamelin (Marian) marie-antoinette.de-bonis-hamelin@hp.com

--- test/testrunner/TestCtrlRunner.js
+++ test/testrunner/TestCtrlRunner.js
@@ -1,6 +1,9 @@
 enyo.kind({
 	name: "Ares.TestCtrlRunner",
 	kind: enyo.Component,
+	components: [
+		{name: "infoPopup", kind: "InfoPopup", onCancel: "cancel"}
+	],
 	index: 0,
 	create: function() {
 		this.inherited(arguments);
@@ -12,6 +15,8 @@ enyo.kind({
 			var aresRunner = this.createComponent({name: test.prototype.kindName, kind: Ares.TestProxyReporter, onFinishAll: "next", aresObj: this.aresObj});
 			aresRunner.runTests();
 		} else {
+			// Popup to inform results are available into console when executing tests on IE	
+			if (aresTestW === null) this.$.infoPopup.show();
 			// No new test to run, we need to remove the temp test/root directory
 			var req = new enyo.Ajax({
 				url: '/res/tester',
@@ -25,3 +30,30 @@ enyo.kind({
 		}
 	}
 });
+
+enyo.kind({
+		name: "InfoPopup",
+		kind: "onyx.Popup",
+		events: {
+			onCancel: ""
+		},
+		modal: true,
+		centered: true,
+		floating: true,
+		components: [
+			{tag: "h3", content: "The results of the Ares TestRunner are available in the console!!"},
+			{tag: "br"},
+			{kind: "FittableColumns", components: [
+				{name: "cancelButton", kind: "onyx.Button", classes: "onyx-affirmative", content: "cancel", ontap: "deleteCancel"},
+				{fit: true},
+			]}
+		],
+		create: function() {
+			this.inherited(arguments);
+		},
+		deleteCancel: function(inSender, inEvent) {
+			this.hide();
+			this.doCancel();
+		}
+});
+
