b40fa6584cc50a47b7de8c34fdf68f9d59f1de0d
Francois-Xavier KOWALSKI <francois-xavier.kowalski@hp.com>
2013-03-29 14:38:54 +0100

    ENYO-2089: forbid running on node < 0.8.21 (run-time check)
    Enyo-DCO-1.1-Signed-off-by: Francois-Xavier KOWALSKI <francois-xavier.kowalski@hp.com>

--- hermes/lib/checkNodeVersion.js
+++ hermes/lib/checkNodeVersion.js
@@ -1,8 +1,8 @@
 (function() {
 
-	var version = process.version.match(/v([0-9]+).([0-9]+)/);
-	if (version[1] != 0 || version[2] < 8 || version[2] > 8) {
-		console.error("Ares ide.js is only supported on Node.js version 0.8.x");
+	var version = process.version.match(/v([0-9]+).([0-9]+).([0-9]+)/);
+	if (version[1] != 0 || version[2] < 8 || version[2] > 8 || version[3] < 21) {
+		console.error("Ares ide.js only works on Node.js version 0.8.x >= 0.8.21");
 		process.exit(1);
 	}
 
