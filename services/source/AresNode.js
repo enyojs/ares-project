80d253b4493fb11e359f62676d00f5049d741f88
vherilierHP <vincent.herilier@hp.com>
2013-05-30 13:45:44 +0200

    ENYO-2151: apply better code style

--- services/source/AresNode.js
+++ services/source/AresNode.js
@@ -13,7 +13,7 @@ enyo.kind({
 		onFolderClick: "",
 		onFileDblClick: "",
 		onAdjustScroll: "",
-		onNodeMove: "",
+		onNodeMove: ""
 	},
 	published: {
 		service: null
@@ -22,7 +22,7 @@ enyo.kind({
 		ondragstart: "dragStart",
 		ondrop: "drop",
 		ondragover: "dragOver",
-		ondragout: "dragOut",
+		ondragout: "dragOut"
 	},
 	
 	// expandable nodes may only be opened by tapping the icon; tapping the content label
@@ -34,18 +34,12 @@ enyo.kind({
 	node: null,
 
 	dragStart: function(inSender, inEvent) {
-		// Prevent MouseEvents in case of us of the draggable attribute
-		//if (inEvent instanceof MouseEvent) return true;
-		
 		if (this.debug) this.log(inSender, "=>", inEvent);
-		
-		// Enable HTML5 drop
-		//inEvent.preventDefault();
 		
 		node = null;
 		
 		// look for the related ares.Node
-		if (inSender.kind == "ares.Node") {
+		if (inSender.kind === "ares.Node") {
 			node = inSender;
 		} else {
 			node = inSender.container;
@@ -59,7 +53,7 @@ enyo.kind({
 		var newParentNode = "";
 		
 		// look for the related ares.Node
-		if (inSender.kind == "ares.Node") {
+		if (inSender.kind === "ares.Node") {
 			newParentNode = inSender;
 		} else {
 			// Control or Image...
@@ -69,22 +63,21 @@ enyo.kind({
 		this.doNodeMove({oldNode: node, newParent: newParentNode});
 		
 		newParentNode.applyStyle("cursor", "default");
-		if (newParentNode.file.isDir && newParentNode.expanded) newParentNode.applyStyle("background-color", null);
+		if (newParentNode.file.isDir && newParentNode.expanded) {
+			newParentNode.applyStyle("background-color", null);
+		}
 		
 		node = null;
 		
 		return true;
 	},
 	dragOver: function(inSender, inEvent) {
-		// Prevent MouseEvents in case of us of the draggable attribute
-		//if (inEvent instanceof MouseEvent) return true;
-		
 		if (this.debug) this.log(inSender, "=>", inEvent);
 		
 		var newParentNode = "";
 		
 		// look for the related ares.Node
-		if (inSender.kind == "ares.Node") {
+		if (inSender.kind === "ares.Node") {
 			newParentNode = inSender;
 		} else {
 			// Control or Image...
@@ -94,47 +87,51 @@ enyo.kind({
 		var nodeFile = node.file;
 		var newParentFile = newParentNode.file;
 		
-		// Applaying the related DnD style
+		// Applying the related DnD style
 		if (nodeFile != newParentFile) {
 			if (newParentFile.isDir) {
 				if (node.container.file.id != newParentFile.id) {
 						if (!nodeFile.isDir || newParentFile.isServer || newParentFile.dir.indexOf(nodeFile.dir) == -1) {
 						newParentNode.applyStyle("cursor", "pointer");
-						if (newParentNode.file.isDir && newParentNode.expanded) newParentNode.applyStyle("background-color", "grey");
 					} else {
 						if (this.debug) this.log("target node is a child node");
 						newParentNode.applyStyle("cursor", "no-drop");
-						if (newParentNode.file.isDir && newParentNode.expanded) newParentNode.applyStyle("background-color", "grey");
 					}
 				} else {
 					if (this.debug) this.log("target node is its own parent node");
 					newParentNode.applyStyle("cursor", "no-drop");
-					if (newParentNode.file.isDir && newParentNode.expanded) newParentNode.applyStyle("background-color", "grey");
 				}
 			} else {
 				if (this.debug) this.log("target node is a file");
 				newParentNode.applyStyle("cursor", "no-drop");
-				if (newParentNode.file.isDir && newParentNode.expanded) newParentNode.applyStyle("background-color", "grey");
 			}
 		} else {
 			if (this.debug) this.log("target node is itself");
 			newParentNode.applyStyle("cursor", "no-drop");
-			if (newParentNode.file.isDir && newParentNode.expanded) newParentNode.applyStyle("background-color", "grey");
 		}
 		
+		if (newParentNode.file.isDir && newParentNode.expanded) {
+			newParentNode.applyStyle("background-color", "grey");
+		}
+				
 		return true;
 	},
 	dragOut: function(inSender, inEvent) {
 		if (this.debug) this.log(inSender, "=>", inEvent);
 		
+		var isNode = null;
+		
 		// look for the related ares.Node to apply the DnD style
-		if (inSender.kind == "ares.Node") {
-			inSender.applyStyle("cursor", "default");	
-			if (inSender.file.isDir && inSender.expanded) inSender.applyStyle("background-color", null);
+		if (inSender.kind === "ares.Node") {
+			isNode = inSender;
 		} else {
 			// Control or Image...
-			inSender.container.applyStyle("cursor", "default");
-			if (inSender.container.file.isDir && inSender.container.expanded) inSender.container.applyStyle("background-color", null);			
+			isNode = inSender.container;
+		}
+		
+		isNode.applyStyle("cursor", "default");
+		if (isNode.file.isDir && isNode.expanded) {
+			isNode.applyStyle("background-color", null);
 		}
 		
 		return true;
@@ -209,11 +206,7 @@ enyo.kind({
 
 				case 1: // file added
 				    if (this.debug) this.log(rfiles[i].name + " was added") ;
-					newControl = this.createComponent( rfiles[i], {kind: "ares.Node", 
-						//FIXME: ENYO-2448 use of draggable attributes to enhance DnD feature
-						//Turn draggable attribute to true
-						//attributes: {draggable: true,},
-						} ) ;
+					newControl = this.createComponent( rfiles[i], {kind: "ares.Node"} ) ;
 					if (this.debug) this.log("updateNodeContent created ", newControl) ;
 					newControl.setService(this.service);
 					nfiles = this.getNodeFiles() ;
@@ -395,5 +388,5 @@ enyo.kind({
 			tracker.dec(); // run only for inner calls to refreshTree
 		}
 		this.debug && this.log("refreshTree done") ;
-	},
+	}
 });
