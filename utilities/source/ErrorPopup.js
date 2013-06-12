94890a947957f8a8018be89a66426bd736f1eb78
Olga Popova <olga.popova@hp.com>
2013-06-12 11:31:20 +0200

    ENYO-2411: make error popup verticaly resizable

--- utilities/source/ErrorPopup.js
+++ utilities/source/ErrorPopup.js
@@ -11,17 +11,17 @@ enyo.kind({
         classes:"ares-classic-popup",
         components: [
             {tag: "div", classes:"title", content: "Error"},
-			{kind: "enyo.Scroller", classes:"ares-small-popup", fit: true, components: [
+			{classes:"ares-error-popup", fit: true, components: [
 				{name: "msg"},
-				{classes:"ares-small-popup-details", components:[
+				{classes:"ares-error-details", components:[
 					{classes:"button", components:[
 						{tag:"label", classes:"label", name: "detailsBtn", content: "Details", ontap: "toggleDetails", showing: false},
 						{name:"detailsArrow", classes:"optionDownArrow", ontap: "toggleDetails", showing: false},
 						{name: "detailsDrw", kind: "onyx.Drawer", open: false, showing:false, classes:"ares-error-drawer", components: [
 							{name: "detailsTxt", kind: "onyx.TextArea", disabled: true, fit:true, classes:"ares-error-text"}
 						]}
-					]}
 				]}
+			]},
 			]},
 			{kind: "onyx.Toolbar", classes:"bottom-toolbar", components: [
 				{name: "okButton", kind: "onyx.Button", content: "Close", ontap: "hideErrorPopup"}
@@ -38,7 +38,7 @@ enyo.kind({
 			this.$.detailsBtn.show();
 			this.$.detailsArrow.show();
 			this.$.detailsDrw.show();
-			this.$.detailsDrw.setOpen(false);
+			this.$.detailsDrw.setOpen(true);
 			this.$.detailsTxt.setValue(this.details);
 		} else {
 			this.$.detailsBtn.hide();
