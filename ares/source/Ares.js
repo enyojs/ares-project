f31453f57f696706b1ee0181d497c9fe7a80136f
Olga Popova <olga.popova@hp.com>
2013-06-19 09:33:41 +0200

    ENYO-2402: try to repare Dnd bug

--- ares/source/Ares.js
+++ ares/source/Ares.js
@@ -13,7 +13,7 @@ enyo.kind({
 				{kind: "Deimos", classes:"ares-full-display", onCloseDesigner: "closeDesigner", onDesignerUpdate: "designerUpdate", onUndo: "designerUndo", onRedo: "designerRedo"}
 			]}
 		]},
-		{kind: "Slideable", layoutKind: "FittableRowsLayout", classes: "onyx ares-files-slider ares-full-display", axis: "v", value: 0, min: -500, max: 0, unit: "px", onAnimateFinish: "finishedSliding", components: [
+		{kind: "Slideable", layoutKind: "FittableRowsLayout", classes: "onyx ares-files-slider", axis: "v", value: 0, min: -500, max: 0, unit: "px", onAnimateFinish: "finishedSliding", components: [
 			{name: "projectView", kind: "ProjectView", fit: true, classes: "onyx", onFileDblClick: "openDocument", onProjectSelected: "projectSelected"},
 			{name: "bottomBar", classes:"ares-bottom-bar", kind: "DocumentToolbar",
 				onToggleOpen: "toggleFiles",
