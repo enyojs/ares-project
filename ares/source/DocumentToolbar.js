28bc5c02d940a598ebe45ea5b717fb245ddef1fb
Dominique Dumont <dominique.dumont@hp.com>
2013-07-01 16:51:17 +0200

    ENYO-2266: replaced dynamic buttons with TabBar from Onyx

--- ares/source/DocumentToolbar.js
+++ ares/source/DocumentToolbar.js
@@ -23,40 +23,30 @@ enyo.kind({
 			ontap: "doToggleOpen",
 			components: [
 				{kind: "onyx.Grabber"},
-				{name: "tabs", classes: "ares-docbar-tabs", kind: "onyx.RadioGroup"}
+				{
+					name: "tabs",
+					kind: "onyx.TabBar",
+					onTabChanged: 'switchFile',
+					onTabRemove: 'closeFile'
+				}
 			]
 		}
 	],
 	tabs: {},
 	createFileTab: function(name, id) {
-		var c = this.$.tabs.createComponent(
+		var c = this.$.tabs.addTab(
 			{
-				kind: "AresTab",
-				fileId: id,
-				components: [
-					{content: name, classes: "ares-tab-label"},
-					{
-						name: "close-"+id,
-						kind: "onyx.IconButton",
-						classes: "ares-doc-close",
-						src: "$lib/onyx/images/progress-button-cancel.png",
-						fileId: id,
-						ontap: "closeFile"
-					}
-				],
-				ontap: "switchFile"
-			},
-			{owner: this}
-		).render();
-		this.$.container.reflow();
-		this.tabs[id] = c;
+				caption: name,
+				userId: id // id like home-123f3c8a766751826...
+			}
+		);
 	},
 	switchFile: function(inSender, inEvent) {
-		this.doSwitchFile({id: inSender.fileId});
+		this.doSwitchFile({id: inEvent.data.userId});
 		return true;
 	},
 	activateFileWithId: function(id) {
-		this.tabs[id].setActive(true);
+		this.$.tabs.activate({ userId: id });
 	},
 	closeFile: function(inSender, inEvent) {
 		var id = inSender.fileId;
