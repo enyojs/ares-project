3e7999b51db83b95c04690b420fbb6bd85d4a60b
Francois-Xavier Kowalski <francois-xavier.kowalski@hp.com>
2013-08-12 15:55:48 +0200

    ENYO-2879: drop usage of script wrapper to execute jshint at "npm test" time

---
+++ scripts/test.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+var fork = require('child_process').fork;
+var path = require('path');
+process.chdir(path.join(__dirname, '..'));
+var cmd = path.join('node_modules', 'jshint', 'bin', 'jshint')
+var args = ['.'];
+console.log('> node ' + cmd + ' ' + args.join(' '));
+fork(cmd, args);
