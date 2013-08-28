da6666e376479ff6c9dfb9e2a2c402523e6f099a
Marian de Bonis <marie-antoinette.de-bonis-hamelin@hp.com>
2013-08-28 16:41:51 +0200

    ENYO-2902: enyo.log() added

--- test/testrunner/TestRunner.js
+++ test/testrunner/TestRunner.js
@@ -14,7 +14,10 @@ enyo.kind({
 	next: function() {
 		var test = Ares.TestSuite.tests[this.index++];
 		if (test) {
+			enyo.log("STARTING TEST SUITE ", test.prototype.kindName);
 			this.createComponent({name: test.prototype.kindName, kind: Ares.TestReporter, onFinishAll: "next"}).render();
+		} else {
+			enyo.log("TEST RUNNER FINISHED");
 		}
 	}
 });
