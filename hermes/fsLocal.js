a7bca5aaa27fdbab1eaccd905233fa8e9a910bf6
Junil Kim <junilkim@Junil-ui-MacBook-Pro.local>
2013-09-02 11:47:45 +0900

    stat.ino is not working on Windows, so added mtime comparing condition to identify a same file

--- hermes/fsLocal.js
+++ hermes/fsLocal.js
@@ -459,7 +459,8 @@ FsLocal.prototype._changeNode = function(req, res, op, next) {
 					}).bind(this));
 				}).bind(this));
 			} else {
-				if (req.method.match(/MOVE/i) && (srcStat.ino === dstStat.ino)) {
+				if (req.method.match(/MOVE/i) && (srcStat.ino === dstStat.ino) 
+									&& (srcStat.mtime.getTime() === dstStat.mtime.getTime())) {
 						op(srcPath, dstPath, (function(err) {
 							this._propfind(err, dstRelPath, 1 /*depth*/, function(err, content) {
 								next(err, {
