75cd71e1870383663d484037fc1a4aed65001e45
adicherafik <mahmoud-rafik.adiche@hp.com>
2013-10-22 14:04:04 +0200

    ENYO-2788: add a mecanism to the stat() function in "ProjectWizard" in order to define the first tab to be displayed when the Pop-up is shown.

--- project-view/source/ProjectWizard.js
+++ project-view/source/ProjectWizard.js
@@ -387,6 +387,7 @@ enyo.kind({
 	targetProject: null,
 	chooser: null,
 	checker: null,
+	displayedTab: undefined,
 
 	create: function() {
 		ares.setupTraceLogger(this);	// Setup this.trace() function according to this.debug value
@@ -395,9 +396,12 @@ enyo.kind({
 	/**
 	 * Step 1: start the modification by showing project properties widget
 	 */
-	start: function(target) {
+	start: function(target, inDisplayedTab) {
 		if (target) {
 			var config = target.getConfig();
+
+			// Define the tab that will be shown when the Pop-up is displayed
+			this.displayedTab = inDisplayedTab;
 			
 			this.targetProject = target ;
 			this.$.propertiesWidget.setTargetProject(target);
@@ -538,7 +542,13 @@ enyo.kind({
 	fileChoosersChecked: function (inSender, inEvent) {
 		this.trace(inSender, "=>", inEvent);
 		this.show();
-		this.$.propertiesWidget.setDefaultTab();
+
+		if (this.displayedTab !== undefined) {
+			this.$.propertiesWidget.setDisplayedTab(this.displayedTab);
+		} else {
+			this.$.propertiesWidget.setDefaultTab();
+		}
+		
 		return true;
 	}
 	
