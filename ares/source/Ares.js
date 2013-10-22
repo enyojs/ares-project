4cf3b509c1c846679d5628e9a0a514b6701cf41b
Olga Popova <olga.popova@hp.com>
2013-10-22 17:21:25 +0200

    ENYO-3392:  do select project only if the opened file comes from another project

--- ares/source/Ares.js
+++ ares/source/Ares.js
@@ -427,9 +427,14 @@ enyo.kind({
 	switchFile: function(inSender, inEvent) {
 		var d = Ares.Workspace.files.get(inEvent.id);
 		if (d) {
-			var project = Ares.Workspace.projects.get(d.getProjectData().id);
-			ComponentsRegistry.getComponent("projectList").selectInProjectList(project);
-			this.switchToDocument(d);
+			//select project if the file(d) comes from another project then the previous file
+			if (this.activeDocument.id && d !== this.activeDocument){
+				var project = Ares.Workspace.projects.get(d.getProjectData().id);
+				if(project){
+					ComponentsRegistry.getComponent("projectList").selectInProjectList(project);
+				}
+			}
+			this.switchToDocument(d);		
 		} else if (this.debug) {
 			throw("File ID " + d + " not found in cache!");
 		}
