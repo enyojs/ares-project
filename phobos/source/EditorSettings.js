4bbc0e956c153824b51068923a148eb1357dc6df
micro-tech <john.microtech@gmail.com>
2014-01-06 00:39:26 -0500

    Set width of input box for F keys

--- phobos/source/EditorSettings.js
+++ phobos/source/EditorSettings.js
@@ -132,7 +132,7 @@ enyo.kind({
 			components: [
 				{kind: "Control", classes: "ace-input-popup", name: "altInputbox", components: [
 					{kind: "onyx.InputDecorator", classes: "ace-input-textarea", name: "inputDecorator", components: [
-						{kind: "onyx.TextArea", placeholder: "Enter text here", name: "textArea"}
+						{kind: "onyx.TextArea", style: "Width: 100%;", placeholder: "Enter text here", name: "textArea"}
 					]}
 				]},
 				{kind: "onyx.Toolbar", classes:"bottom-toolbar", components: [
