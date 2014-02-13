1050760a9ac505de70324e286b56f2c4c82b581b
Francois-Xavier KOWALSKI <francois-xavier.kowalski@hp.com>
2014-02-13 15:50:17 +0100

    ENYO-2627: appease Lord Jshint III
    Enyo-DCO-1.1-Signed-off-by: Francois-Xavier KOWALSKI <francois-xavier.kowalski@hp.com>

--- scripts/postpublish.js
+++ scripts/postpublish.js
@@ -1,10 +1,8 @@
 /*jshint node:true*/
-/*global console, process, require*/
-var path = require('path'),
-    shell = require('shelljs'),
+/*global console, require*/
+var shell = require('shelljs'),
     deploys = require('./common.js').deploys;
 
-var cmd = [process.env.npm_node_execpath, path.join(__dirname, "..", "enyo", "tools", "deploy.js") /*, "-v"*/].join(' ');
 deploys.forEach(function(app) {
 	console.log("> rm -rf " + app.o);
 	shell.rm("-rf", app.o);
