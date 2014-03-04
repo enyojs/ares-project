df336c0c25bcaa83814e9fa4795e1c4d33029ad4
Vincent Herilier <vincent.herilier@hp.com>
2014-03-04 18:02:54 +0100

    ENYO-3842: generate one and only one minified context "_ares"

--- scripts/common.js
+++ scripts/common.js
@@ -1,6 +1,5 @@
 /*jshint node:true*/
 /*global exports*/
 exports.deploys = [
-	{o: '_ide', p: './package.js', e: './enyo'},
-	{o: '_preview', s: './source/preview', e: '../../enyo', l: '../../lib'}
+	{o: '_ares', p: './package.js', e: './enyo'}
 ];
